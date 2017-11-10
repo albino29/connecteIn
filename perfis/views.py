8753d437ee9d0861d8b5c38176ad9058e49b988c
albino29 <bruhalbino29@gmail.com>
2017-11-10 00:13:49 -0200

    autenticando usuario com django middleware

--- perfis/views.py
+++ perfis/views.py
@@ -1,26 +1,36 @@
 from django.shortcuts import render, redirect
 from perfis.models import Perfil, Convite
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def index(request):
-	return render(request, 'index.html', {'perfis' : Perfil.objects.all(), 'perfil_logado' : get_logged(request)})
+    return render(request, 'index.html', {'perfis': Perfil.objects.all(), 'perfil_logado': get_logged(request)})
 
+
+@login_required
 def exibir(request, perfil_id):
-	perfil = Perfil.objects.get(id=perfil_id)
-	perfil_logado = get_logged(request)
-	is_contato = perfil in perfil_logado.contatos.all()
-	return render(request, 'perfil.html', {"perfil" : perfil, 'is_contato' : is_contato})
+    perfil = Perfil.objects.get(id=perfil_id)
+    perfil_logado = get_logged(request)
+    is_contato = perfil in perfil_logado.contatos.all()
+    return render(request, 'perfil.html', {"perfil": perfil, 'is_contato': is_contato})
 
+
+@login_required
 def convidar(request, perfil_id):
-	perfil_a_convidar = Perfil.objects.get(id=perfil_id)
-	perfil_logado = get_logged(request)
-	perfil_logado.convidar(perfil_a_convidar)
-	return redirect('index')
+    perfil_a_convidar = Perfil.objects.get(id=perfil_id)
+    perfil_logado = get_logged(request)
+    perfil_logado.convidar(perfil_a_convidar)
+    return redirect('index')
 
+
+@login_required
 def get_logged(request):
-	return Perfil.objects.get(id=1)
+    return request.user.perfil
 
+
+@login_required
 def aceitar(request, convite_id):
-	convite = Convite.objects.get(id=convite_id)
-	convite.aceitar()
-	return redirect('index')
+    convite = Convite.objects.get(id=convite_id)
+    convite.aceitar()
+    return redirect('index')
